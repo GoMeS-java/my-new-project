3ba26a675ed377c918003fe6d4420c2539076069
Илья Ромашов <ugghhtb@gmail.com>
2024-11-22 08:32:04 +0300

    первый коммит

--- src/ClassesAndObjects.java
+++
@@ -1,9 +0,0 @@
-public class ClassesAndObjects {
-    public static void main(String[] args){
-        StringBuilder sb = new StringBuilder("Hello");
-        System.out.println(sb.toString());
-        sb.append(" my").append(" friend");
-        System.out.println(sb.toString());
-    }
-}
-
--- src/peremena.java
+++
@@ -1,5 +0,0 @@
-public class peremena {
-    public static void main(String[]args){
-
-    }
-}
